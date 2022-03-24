e11fe325ad38b7cad33e550773c707fa27c03f9c
Shahosol <soltani.shaho@gmail.com>
2022-03-24 20:16:44 +0200

    Tehtävä 2.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
-import { useState } from 'react'
 import './App.css'
 import Course from './Course'
 
 function App() {
   const course = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
     {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
   ]
 
@@ -27,6 +56,7 @@ function App() {
       <header className="App-header">
         <h1>Seoul</h1>
         <Course course={course} totalExercises={totalExercises} />
+
       </header>
     </div>
   )
--- src/Course.jsx
+++ src/Course.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function Course({ course, totalExercises }) {
   return (
     <div>
-        <p>{course.map(course => <p key={course.name}>{course.name} {course.exercises}</p>)}</p>
+        {/* <p>{course.map(course => <p key={course.name}>{course.name} {course.exercises}</p>)}</p>
+        <p>{course.map(course => <p key={course.name}>{course.parts[0].name} {course.exercises}</p>)}</p> */}
+
+        {course.map(course => {
+          return (
+          <li key={course.name}>
+            {course.name} {course.parts[0].name} {course.parts[0].exercises}
+            {course.parts[1].name} {course.parts[1].exercises}
+          </li>
+          )
+        })}
+
+        
+        
         <p>Total exercises: { totalExercises }</p>
     </div>
   )
